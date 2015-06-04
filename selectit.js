c0a57f0fb02a262411d6057bf7a9bcbea5807f0a
NMcCloud <kenneth.gendron@gmail.com>
2015-06-04 16:16:44 -0700

    Update selectit.js

--- selectit.js
+++ selectit.js
@@ -321,6 +321,7 @@
                     delete this.keypressevent;
                 },
                 blur: function(event) {
+                	console.log("blur");
                     if (this.cancelBlur) {
                         delete this.cancelBlur;
                         return;
